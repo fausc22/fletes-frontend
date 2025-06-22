0e35ec4d334c3f3b9250c5e072776edf149fe109
Fausto <faausc@gmail.com>
2025-06-22 16:25:14 -0300

    pwa config para prod

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
+  disable: false, // IMPORTANTE: debe ser false en producción
+  buildExcludes: [/middleware-manifest\.json$/]
 });
 
 module.exports = withPWA({
